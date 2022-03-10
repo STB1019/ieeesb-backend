4bdfcf45ea856a8c414fbf4d9d085c3aa1460947
AlessandroMuscio <muscioalex30@gmail.com>
2022-03-10 11:12:23 +0100

    feat: added the possibility to delete an article

--- app.js
+++ app.js
@@ -4,53 +4,29 @@
 // di 'mongoose', tuttavia entrambi ci semplificano la vita rispetto ai pacchetti
 // di default di 'NodeJS' quindi non vedo perché non usarli.
 const express = require("express");
-const mongoose = require("mongoose");
-const User = require("./models/user");
 const config = require("./config.json");
+const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 const dbURI = config.dbUri
 
-// Passandogli il link del database mi connetto ad esso. Infine il metodo 'connect()'
-// è una funzione asincrona ('https://www.youtube.com/watch?v=ZcQyJ-gxke0' qui è
-// spiegato bene cosa vuol dire). In breve, JavaScript, invece che aspettare che il
-// metodo finisca la sua esecuzione, continua ad eseguire il resto del file e noi
-// volendo possiamo aggiungere una funzione di callback per dirgli cosa fare una
-// volta che il metodo ha concluso la sua esecuzione.
-mongoose.connect(dbURI)
-// Con il metodo 'then()' possiamo specificare la funzione di callback da dare ad
-// una funzione asincrona. In questo caso faccio in modo che il server inizi ad
-// ascoltare le richieste solo dopo che la connessione con il DB è andata a buon fine.
-// Mentre usiamo 'catch()' per "prendere" eventuali errori.
-.then((result) => app.listen(PORT, console.log(`Server started on port ${PORT}...`)))
-.catch((error) => console.log(error));
+const articlesRoute = require("./src/routes/articlesRoutes");
+
+app.listen(PORT, console.log(`Server started on port ${PORT}...`)) // TO REMOVE
 
 // Utilizziamo il metodo 'use()' per creare un "middleware", codice che viene eseguito
 // indipendentemente dalla route che viene richiesta. In questo caso creiamo un
 // middleware che, in caso siano presenti, converte i dati inviati da un form HTML in un
 // oggetto JavaScript. Questi poi vengono salvati all'interno del body della richiesta nel
 // metodo che gestisce la specifica route a cui la richiesta è stata effettuata.
-app.use(express.urlencoded({ extended: true }));
+// Il pezzo qui sopra lo tengo perché può tornare utile ma usando "multer" questo lo fai
+// lui in automatico.
 
-// Metodo che gestisce una richiesta di tipo 'POST' alla route '/post'.
-app.post("/post", (req, res) => {
-  // Come dicevo prima, prendo i dati inviati dal form, che sono stati salvati dal
-  // middleware nel body della richiesta, e li salvo in una variabile.
-  const POST = req.body;
-  // Creo un oggetto di tipo User, come attributo il costruttore prende un oggetto che
-  // è uguale allo Schema che abbiamo definito nel model.
-  const user = new User({
-    username: POST["username"],
-    password: POST["password"]
-  });
+const base64_encode = (file) => {
+  let bitmap = fs.readFileSync(file);
 
-  // Con il metodo 'save()' salviamo nella collections questo oggetto. Anche save è
-  // asincrono, quindi stesso discorso di 'connect()' con 'then()' e 'catch()'.
-  user.save()
-  .then((result) => res.send("<p>Aggiunta con successo l'oggetto " + result + " </p>"))
-  .catch((error) => console.log(error));
+  return Buffer.from(bitmap).toString("base64");
+};
 
-  // Ritorno alla homepage
-  // res.redirect("/");
-});
+app.use(articlesRoute)
