4ecb3bc8619be1c4b6952875b99e8028f2bd3d08
AlessandroMuscio <muscioalex30@gmail.com>
2022-05-24 16:22:26 +0200

    refactor: added more error handling, stil in progress

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -10,38 +10,88 @@ const fs = require("fs");
 // continua ad eseguire il resto del file e noi volendo possiamo aggiungere una funzione di
 // callback per dirgli cosa fare una volta che il metodo ha concluso la sua esecuzione.
 mongoose.connect(dbURI)
-// Con il metodo 'then()' possiamo specificare la funzione di callback da dare ad una funzione
-// asincrona. In questo caso faccio in modo che il server inizi ad ascoltare le richieste solo dopo
-// che la connessione con il DB è andata a buon fine. Mentre usiamo 'catch()' per "prendere"
-// eventuali errori.
-.then(() => console.log("DB connected!"))
-.catch((error) => console.log(error));
+  // Con il metodo 'then()' possiamo specificare la funzione di callback da dare ad una funzione
+  // asincrona. In questo caso faccio in modo che il server inizi ad ascoltare le richieste solo dopo
+  // che la connessione con il DB è andata a buon fine. Mentre usiamo 'catch()' per "prendere"
+  // eventuali errori.
+  .then(() => console.log("DB connected!"))
+  .catch((error) => console.log(error));
+
+let confirmationMessages = {
+  DELETE: "DELETE request successful"
+};
 
 let errorMessages = {
   NOTFOUND: "The researched article wasn't found.",
   CAST: "The id of the requested article is in the wrong format.",
+  NEGATIVE: "Page number cannot be negative!",
   DEFAULT: "Something went wrong!\nPlease try again later."
-}
+};
 
 const controller = {
-  // Metodo che gestisce una richiesta di tipo 'DELETE' alla route '/articles'.
+  /**
+  * Metodo che gestisce una richiesta di tipo "DELETE" al percorso "/articles/:id".
+  *
+  * @param {*} req Rappresenta la richiesta fatta al server.
+  * @param {*} res Rappresenta la risposta del server.
+  */
   deleteArticle: (req, res) => {
-    // Grazie a questo posso prendere i route parameters di questa route
+    /**
+    * Per prima cosa prendo il "route parameters" dal percorso a cui è stata fatta la richiesta,
+    * quel ":id" serve solo a dare il nome al parametro che assumerà il valore inserito nel
+    * percorso.
+    */
     let id = req.params.id;
 
-    // Elimino l'articolo dal database cercandolo per il suo id
-    Article.findByIdAndDelete(id)
-    // Invio come risposta una stringa "OK!"
-    .then(() => res.send("DELETE request executed!"))
-    // Stampo il risultato in console in caso di errore (da rivedere)
-    .catch((error) => console.log(error));
+    /**
+     * Utilizzo il metodo "findById()" il quale, dato l'id di un articolo lo ricerca all'interno
+     * del database.
+     * Posso verificarsi 4 casi:
+     * 1. La ricerca va a buon fine e l'articolo viene trovato, viene eliminata l'immagine salvata
+     * e poi viene eseguito un 'findByIdAndDelete()' che cancella l'articolo dal database e poi,
+     * con status code "200 OK" (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200),
+     * invia un messaggio di conferma; oppure, in caso di errore, viene impostato lo status code
+     * "500 Internal Server Error" (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500)
+     * e viene inviato un messaggio d'errore generico come json;
+     * 2. Non viene trovato l'articolo trovato ma la ricerca non da errori, significa che
+     * l'articolo cercato non è stato trovato o non esiste, vine impostato lo status code
+     * "404 Not Found" (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404) e viene
+     * inviato un messaggio d'errore come json;
+     * 3. Il metodo genera un errore, nello specifico se si tratta di un "CastError", significa che
+     * la richiesta è stata posto in maniera errata e quindi viene impostato lo status code
+     * "400 Bad Request" (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400) e viene
+     * inviato un messaggio d'errore come json;
+     * 4. In caso non si tratti di un "CastError" ma di un altro errore viene impostato lo status
+     * code "500 Internal Server Error"
+     * (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500) e viene inviato un messaggio
+     * d'errore generico come json.
+     */
+    Article.findById(id)
+      .then((result) => {
+        if (result) {
+          fs.unlinkSync(result.thumbnail);
+
+          Article.findByIdAndDelete(id, (error) => {
+            if (error)
+              console.error(error);
+            else
+              res.status(200).json(confirmationMessages.DELETE);
+          })
+        } else
+          res.status(404).json(errorMessages.NOTFOUND);
+      })
+      .catch((error) => {
+        if (error.name == "CastError")
+          res.status(400).json(errorMessages.CAST);
+        else
+          res.status(500).json(errorMessages.DEFAULT);
+      });
   },
   getArticles: (req, res) => {
     let data = req.query;
 
     let page = parseInt(data.page, 10);
     let step = parseInt(data.step, 10);
-
     let skip = (page - 1) * step;
 
     // Il metodo find ritorna una lista con tutti gli articoli presenti nel DB
@@ -51,12 +101,12 @@ const controller = {
     /* Non capisco perché così non funziona, ma se metto i "magic number" al posto delle variabili
     funziona :( */
     Article.find().skip(skip).limit(step)
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
   /**
   * Metodo che gestisce una richiesta di tipo "GET" al percorso "/articles/:id".
@@ -94,13 +144,13 @@ const controller = {
      */
     Article.findById(id)
       .then((result) => {
-        if(result)
+        if (result)
           res.status(200).json(result);
         else
           res.status(404).json(errorMessages.NOTFOUND);
       })
       .catch((error) => {
-        if(error.name == "CastError")
+        if (error.name == "CastError")
           res.status(400).json(errorMessages.CAST);
         else
           res.status(500).json(errorMessages.DEFAULT);
@@ -143,7 +193,7 @@ const controller = {
      * "thumbnail" avente come valore il percorso di dove è stata salvata la thumbnail e poi
      * procedo con il creare l'article secondo lo schema specificato.
      */
-    Object.assign(data, {thumbnail: thumbnailPath});
+    Object.assign(data, { thumbnail: thumbnailPath });
     article = new Article(data);
 
     /**
@@ -152,29 +202,29 @@ const controller = {
      * un messaggio d'errore.
      */
     article.save()
-    .then(() => res.status(201).json(article._id))
-    .catch((error) => {
-      /**
-       * Per prima cosa prendo il messaggio d'errore che ho specificato nello schema, tuttavia
-       * "mongoose" aggiunge altro di non necessario all'interno del messaggio e di conseguenza
-       * utilizziamo un "substr()" per avere solo il messaggio finale.
-       */
-      let errorMessage = error.message;
-      let message = errorMessage.substr(errorMessage.lastIndexOf(":")+2);
+      .then(() => res.status(201).json(article._id))
+      .catch((error) => {
+        /**
+         * Per prima cosa prendo il messaggio d'errore che ho specificato nello schema, tuttavia
+         * "mongoose" aggiunge altro di non necessario all'interno del messaggio e di conseguenza
+         * utilizziamo un "substr()" per avere solo il messaggio finale.
+         */
+        let errorMessage = error.message;
+        let message = errorMessage.substr(errorMessage.lastIndexOf(":") + 2);
 
-      /**
-       * Qui verifichiamo se si tratta di un'errore di validazione, in caso affermativo ritorniamo
-       * l'errore settando lo status code "422 Unprocessable Entity"
-       * (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422). In caso contrario
-       * ritorniamo l'errore settando lo status code "500 Internal Server Error"
-       * (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500).
-       */
-      if (error.name == "ValidationError") {
-        res.status(422).json(message);
-      } else {
-        res.status(500).json(message);
-      }
-    });
+        /**
+         * Qui verifichiamo se si tratta di un'errore di validazione, in caso affermativo ritorniamo
+         * l'errore settando lo status code "422 Unprocessable Entity"
+         * (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422). In caso contrario
+         * ritorniamo l'errore settando lo status code "500 Internal Server Error"
+         * (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500).
+         */
+        if (error.name == "ValidationError") {
+          res.status(422).json(message);
+        } else {
+          res.status(500).json(message);
+        }
+      });
   },
   // Metodo che gestisce una richiesta di tipo 'PATCH' alla route '/articles/:id'.
   patchArticle: (req, res) => {
@@ -186,10 +236,10 @@ const controller = {
     let objectID = req.params.id;
     let getAndDeleteArticle = () => {
       Article.findById(objectID)
-      .then((result) => {
-        let path = result.thumbnail;
-        fs.unlinkSync(path);  
-      });
+        .then((result) => {
+          let path = result.thumbnail;
+          fs.unlinkSync(path);
+        });
     };
 
     // Genero il percorso dove salvare l'immagine (da rivedere)
@@ -203,7 +253,7 @@ const controller = {
     else
       sharp(file.buffer).toFile(path);
 
-    let thumbnail = {thumbnail: path};
+    let thumbnail = { thumbnail: path };
     Object.assign(data, thumbnail);
 
     Article.findOneAndUpdate(
@@ -211,12 +261,12 @@ const controller = {
       data,
       { new: true }
     )
-    .then(() => {
-      getAndDeleteArticle();
-      res.send("PATCH request executed!");
-    })
-    // Stampo il risultato in console in caso di errore (da rivedere)
-    .catch((error) => console.log(error));;
+      .then(() => {
+        getAndDeleteArticle();
+        res.send("PATCH request executed!");
+      })
+      // Stampo il risultato in console in caso di errore (da rivedere)
+      .catch((error) => console.log(error));;
   }
 };
 
